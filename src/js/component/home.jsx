88a688b6fccd1e504624ad8c44d56954b6631c4f
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-04-29 23:47:01 +0000

    u1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,53 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+const [counter, setCounter] =useState(0);
+const [userInput, setUserInput] = useState("");
+let number = 1 *3
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+			<h1 className="text-center mt-5">Count: {counter}</h1>
+			<button onClick={() => setCounter(counter + number)}> + </button>
+			<button onClick={() => setCounter(counter -1)}> - </button>
+			<button onClick={() => setCounter(0)}> Reset </button>
+
+<div>
+             <h1>You name is: {userInput ? userInput : "NULL"}</h1>
+
+
+<input 
+type="text"
+value={userInput}
+placeholder="Enter firstname"
+onChange={(e) => setUserInput(e.target.value)}
+/>
+</div>
+</div>
 	);
 };
 
 export default Home;
+
+// ****************************
+// import React from "react";
+// import { useState } from "react";
+
+
+// const Home = () => {
+// const [counter, setCounter] =useState(0);
+// let number = 1 *3
+// 	return (
+// 		<div className="text-center">
+// 			<h1 className="text-center mt-5">Count: {counter}</h1>
+// 			<button onClick={() => setCounter(counter + number)}> + </button>
+// 			<button onClick={() => setCounter(counter -1)}> - </button>
+// 			<button onClick={() => setCounter(0)}> Reset </button>
+
+// 		</div>
+// 	);
+// };
+
+// export default Home;
+
