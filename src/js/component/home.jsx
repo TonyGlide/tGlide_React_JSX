07b7029e711af059f14d6ad6952722788580b6d0
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-04-29 23:53:15 +0000

    up2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,30 +1,13 @@
 import React from "react";
 import { useState } from "react";
 
-
 const Home = () => {
-const [counter, setCounter] =useState(0);
-const [userInput, setUserInput] = useState("");
-let number = 1 *3
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Count: {counter}</h1>
-			<button onClick={() => setCounter(counter + number)}> + </button>
-			<button onClick={() => setCounter(counter -1)}> - </button>
-			<button onClick={() => setCounter(0)}> Reset </button>
-
 <div>
-             <h1>You name is: {userInput ? userInput : "NULL"}</h1>
+	test
+</div> 
 
-
-<input 
-type="text"
-value={userInput}
-placeholder="Enter firstname"
-onChange={(e) => setUserInput(e.target.value)}
-/>
-</div>
-</div>
 	);
 };
 
@@ -51,3 +34,37 @@ export default Home;
 
 // export default Home;
 
+
+
+// *****************************
+// import React from "react";
+// import { useState } from "react";
+
+
+// const Home = () => {
+// const [counter, setCounter] =useState(0);
+// const [userInput, setUserInput] = useState("");
+// let number = 1 *3
+// 	return (
+// 		<div className="text-center">
+// 			<h1 className="text-center mt-5">Count: {counter}</h1>
+// 			<button onClick={() => setCounter(counter + number)}> + </button>
+// 			<button onClick={() => setCounter(counter -1)}> - </button>
+// 			<button onClick={() => setCounter(0)}> Reset </button>
+
+// <div>
+//              <h1>You name is: {userInput ? userInput : "NULL"}</h1>
+
+
+// <input 
+// type="text"
+// value={userInput}
+// placeholder="Enter firstname"
+// onChange={(e) => setUserInput(e.target.value)}
+// />
+// </div>
+// </div>
+// 	);
+// };
+
+// export default Home;
